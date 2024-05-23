af542e2fe95eab829a62a385d7baa90f0cfe2a77
pedroOlguin <olguinpedro7@gmail.com>
2024-05-22 22:07:57 -0300

    intentando vincular firebase

--- src/hooks/useProductsByCategory.js
+++ src/hooks/useProductsByCategory.js
@@ -1,41 +1,41 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// import {
-//   collection,
-//   getDocs,
-//   getFirestore,
-//   query,
-//   where,
-// } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
-// export default function useProductsByCategory(categoria) {
-//   const [productos, setProducto] = useState(null);
-//   const [cargando, setCargando] = useState(true);
+export default function useProductsByCategory(categoria) {
+  const [productos, setProducto] = useState(null);
+  const [cargando, setCargando] = useState(true);
 
-//   useEffect(() => {
-//     const bd = getFirestore();
+  useEffect(() => {
+    const bd = getFirestore();
 
-//     const q = query(
-//       collection(bd, "productos"),
-//       where("categoria", "===", categoria)
-//     );
+    const q = query(
+      collection(bd, "productos"),
+      where("categoria", "===", categoria)
+    );
 
-//     getDocs(q)
-//       .then((snapshot) => {
-//         setProducto(
-//           snapshot.docs.map((doc) => ({
-//             categoria: doc.categoria,
-//             ...doc.data(),
-//           }))
-//         );
-//       })
-//       .finally(() => {
-//         setCargando(false);
-//       });
-//   }, [categoria]);
+    getDocs(q)
+      .then((snapshot) => {
+        setProducto(
+          snapshot.docs.map((doc) => ({
+            categoria: doc.categoria,
+            ...doc.data(),
+          }))
+        );
+      })
+      .finally(() => {
+        setCargando(false);
+      });
+  }, [categoria]);
 
-//   return {
-//     productos,
-//     cargando,
-//   };
-// }
+  return {
+    productos,
+    cargando,
+  };
+}
